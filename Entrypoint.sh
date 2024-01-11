b76145ab02df380615e9814ed56037435b2552b0
Samer Aburabie <samer@aburabie.me>
2024-01-11 18:24:47 +0400

    one expose

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "boostrapping started ..."
-wg-quick up /etc/wireguard/wg0.conf &
+#echo "boostrapping started ..."
+#wg-quick up /etc/wireguard/wg0.conf &
 ./app/wgrest --listen '127.0.0.1:8080'
-echo "bootstrapping finished ..."
+#echo "bootstrapping finished ..."
 
 
