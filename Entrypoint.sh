63795d7514b6b720df6337fc22473fcf5a2f4d2c
Samer Aburabie <samer@aburabie.me>
2024-01-11 17:50:44 +0400

    updating the image

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
-echo "starting wgrest..."
-./app/wgrest --listen '127.0.0.1:8080' &
+echo "boostrapping started ..."
+wg-quick up /etc/wireguard/wg0.conf &
+./app/wgrest --listen '127.0.0.1:8080'
+echo "bootstrapping finished ..."
 
-stop () {
-    echo "stopping wg0 ..."
-    wg-quick down wg0
-    exit 0
-}
-trap stop SIGTERM SIGINT SIGQUIT
-
-echo "starting wg0 ..."
-wg-quick up /etc/wireguard/wg0.conf
-sleep infinity &
-wait $!
 
