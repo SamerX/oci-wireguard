260adedc177c7b13c5c12f95f7066dded6ef09b4
Samer Aburabie <samer@aburabie.me>
2024-01-12 19:03:20 +0400

    minor test

--- Entrypoint.sh
+++ Entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "boostrapping started ..."
-wg-quick up /etc/wireguard/wg0.conf &
+# wg-quick up /etc/wireguard/wg0.conf &
 ./app/wgrest --listen '127.0.0.1:8080'
 echo "bootstrapping finished ..."
 
